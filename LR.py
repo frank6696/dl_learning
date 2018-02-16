4210778559fb464e86a79856bea453d9d00329e7
frank6696 <frank6696@126.com>
2018-02-16 22:53:24 +0800

    update

--- LR.py
+++ LR.py
@@ -5,29 +5,26 @@ import matplotlib.pyplot as plt
 
 
 def sigmoid(arr):
-    return 1.0/(1+np.exp(-arr))
+    return 1.0/(1.0+np.exp(-arr))
 
 
-#
+# 梯度下降法
 def gradient_descent(X, y, alpha, max_iter):
-    J = np.arange(max_iter, dtype = float)  # loss function
+    J = np.arange(max_iter, dtype=float)  # loss function
     m,n = X.shape
-    theta = np.ones((n,1))  # 初始化参数
-
+    theta = np.ones((n,1),dtype=float)  # 初始化参数
     for i in range(max_iter):
-        h = sigmoid(np.dot(X,theta))
+        h = sigmoid(np.dot(X,theta.astype(float)))
         J[i] = -(np.sum(y*np.log(h)+(1-y)*np.log(1-h)))/m
         error = h - y
         grad = np.dot(X.T, error)
-        theta -= alpha * grad
-        # print(J[i])
-    print(theta)
-    # plt.plot(range(max_iter), J)
-    # plt.show()
+        theta = theta- alpha * grad
+    plt.plot(range(max_iter), J)
+    plt.show()
 
 
 if __name__ == '__main__':
-
+    # load data
     iris = pd.read_csv('data/iris.csv')
     iris.loc[iris['Species'] == 'setosa','Species'] = 1
     iris.loc[iris['Species'] == 'versicolor','Species'] = 1
